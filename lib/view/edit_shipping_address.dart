85b2ec63e4b324902011247ba9343f0dd7fe9dc1
jigar chovatiya <115682188+Jigarchovatiya@users.noreply.github.com>
2023-05-30 12:58:06 +0530

    30-05-2023

--- lib/view/edit_shipping_address.dart
+++ lib/view/edit_shipping_address.dart
@@ -1,3 +1,4 @@
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:shop_easy/res/common/app_appbar.dart';
@@ -15,6 +16,8 @@ class EditShippingAddress extends StatefulWidget {
 class _EditShippingAddressState extends State<EditShippingAddress> {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppAppbar(
         appBar: AppBar(),
@@ -30,11 +33,11 @@ class _EditShippingAddressState extends State<EditShippingAddress> {
                 Row(
                   children: [
                     SvgPicture.asset("assets/icons/account.svg"),
-                    10.0.addWSpace(),
+                    SizedBox(width: width / 40),
                     const Text("Personal Details", style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400)),
                   ],
                 ),
-                20.0.addHSpace(),
+                SizedBox(height: height / 40),
                 const CommonTextField(
                   labelText: "Home",
                   keyboardType: TextInputType.name,
@@ -43,7 +46,7 @@ class _EditShippingAddressState extends State<EditShippingAddress> {
                   maxLines: 1,
                   autofocus: false,
                 ),
-                20.0.addHSpace(),
+                SizedBox(height: height / 40),
                 const CommonTextField(
                   hintText: "Phone Number",
                   keyboardType: TextInputType.name,
@@ -52,16 +55,55 @@ class _EditShippingAddressState extends State<EditShippingAddress> {
                   maxLines: 1,
                   autofocus: false,
                 ),
-                20.0.addHSpace(),
+                SizedBox(height: height / 40),
                 Row(
                   children: [
                     SvgPicture.asset("assets/icons/location.svg"),
-                    10.0.addWSpace(),
+                    SizedBox(width: width / 40),
                     const Text("Address", style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400)),
                   ],
                 ),
-                20.0.addHSpace(),
-                20.0.addHSpace(),
+                SizedBox(height: height / 40),
+                CommonTextField(
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.arrow_drop_down_outlined, size: 25, color: Colors.black),
+                    onPressed: () {
+                      showCountryPicker(
+                        context: context,
+                        countryListTheme: CountryListThemeData(
+                          flagSize: 25,
+                          backgroundColor: Colors.white,
+                          textStyle: const TextStyle(fontSize: 16, color: Colors.blueGrey),
+                          bottomSheetHeight: 500, // Optional. Country list modal height
+                          //Optional. Sets the border radius for the bottomsheet.
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(20.0),
+                            topRight: Radius.circular(20.0),
+                          ),
+                          //Optional. Styles the search field.
+                          inputDecoration: InputDecoration(
+                            labelText: 'Search',
+                            hintText: 'Start typing to search',
+                            prefixIcon: const Icon(Icons.search),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: const Color(0xFF8C98A8).withOpacity(0.2),
+                              ),
+                            ),
+                          ),
+                        ),
+                        onSelect: (Country country) => debugPrint('Select country: ${country.displayName}'),
+                      );
+                    },
+                  ),
+                  hintText: "Street Name",
+                  keyboardType: TextInputType.name,
+                  obscureText: false,
+                  enableInteractiveSelection: true,
+                  maxLines: 1,
+                  autofocus: false,
+                ),
+                SizedBox(height: height / 40),
                 const CommonTextField(
                   hintText: "Street Name",
                   keyboardType: TextInputType.name,
@@ -70,8 +112,12 @@ class _EditShippingAddressState extends State<EditShippingAddress> {
                   maxLines: 1,
                   autofocus: false,
                 ),
-                20.0.addHSpace(),
-                const CommonTextField(
+                SizedBox(height: height / 40),
+                CommonTextField(
+                  suffixIcon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.arrow_drop_down_outlined, size: 25, color: Colors.black),
+                  ),
                   hintText: "Zip/Postal Code",
                   keyboardType: TextInputType.name,
                   obscureText: false,
@@ -79,7 +125,7 @@ class _EditShippingAddressState extends State<EditShippingAddress> {
                   maxLines: 1,
                   autofocus: false,
                 ),
-                20.0.addHSpace(),
+                SizedBox(height: height / 40),
                 const CommonTextField(
                   hintText: "Additional Information",
                   keyboardType: TextInputType.name,
@@ -89,7 +135,7 @@ class _EditShippingAddressState extends State<EditShippingAddress> {
                   autofocus: false,
                 ),
                 20.0.addHSpace(),
-                Container(height: MediaQuery.of(context).size.height / 4.5),
+                SizedBox(height: height / 300),
                 AppButton(
                   title: "save",
                   onTap: () {
